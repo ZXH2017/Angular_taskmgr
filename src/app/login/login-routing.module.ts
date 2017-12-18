c8291b6854eb19652206949ca072bb6eb9910054
ZXH2017 <564407380@qq.com>
2017-12-18 22:10:02 +0800

    优化登录界面

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,6 +1,6 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {LoginComponent} from './login/login.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
