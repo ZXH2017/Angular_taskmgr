aed757b039296cecfddcbe4bf4be967c595e6ce0
ZXH2017 <564407380@qq.com>
2018-01-04 23:32:19 +0800

    新增Angular动画

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { OverlayContainer } from '@angular/material';
+import { trigger, state, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('square', [
+      state('green', style({ 'background-color': 'green', 'height': '100px', 'transform': 'translateX(0)' })),
+      state('red', style({ 'background-color': 'red', 'height': '50px', 'transform': 'translateX(100%)' })),
+      transition('green=>red', animate('.2s 1s')),
+      transition('red=>green', animate(1000)),
+    ])
+  ]
 })
 export class AppComponent {
+
+  squareState: string;
   darkTheme = false;
 
   constructor(private oc: OverlayContainer) {
@@ -15,5 +26,9 @@ export class AppComponent {
   switchTheme(dark) {
     this.darkTheme = dark;
     this.oc.themeClass = dark ? 'myapp-dark-theme' : null;
+  }
+
+  onClick() {
+    this.squareState = this.squareState === 'red' ? 'green' : 'red';
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MdSidenavModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -20,6 +21,7 @@ import { TaskModule } from './task/task.module';
     LoginModule,
     ProjectModule,
     TaskModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
