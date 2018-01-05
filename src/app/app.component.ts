5b0c1827d70aa864a814305072f5793594339879
ZXH2017 <564407380@qq.com>
2018-01-05 22:03:57 +0800

    了解缓动函数和关键帧

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { OverlayContainer } from '@angular/material';
-import { trigger, state, transition, style, animate } from '@angular/animations';
+import { trigger, state, transition, style, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +8,39 @@ import { trigger, state, transition, style, animate } from '@angular/animations'
   styleUrls: ['./app.component.scss'],
   animations: [
     trigger('square', [
-      state('green', style({ 'background-color': 'green', 'height': '100px', 'transform': 'translateX(0)' })),
-      state('red', style({ 'background-color': 'red', 'height': '50px', 'transform': 'translateX(100%)' })),
-      transition('green=>red', animate('.2s 1s')),
-      transition('red=>green', animate(1000)),
+      state('green', style({ 'background-color': 'green', 'height': '100px', 'transform': 'translateY(-100%)' })),
+      state('red', style({ 'background-color': 'red', 'height': '100px', 'transform': 'translateY(100%)' })),
+      transition('green=>red', animate('.8s cubic-bezier(0.68,-0.55,0.265,1.55)')),
+      transition('red=>green', animate(5000,keyframes([
+        style({transform:'translateY(100%)'}),
+        style({transform:'translateY(95%)'}),
+        style({transform:'translateY(90%)'}),
+        style({transform:'translateY(88%)'}),
+        style({transform:'translateY(80%)'}),
+        style({transform:'translateY(50%)'}),
+        style({transform:'translateY(40%)'}),
+        style({transform:'translateY(10%)'}),
+        style({transform:'translateY(0)'}),
+        style({transform:'translateY(-20%)'}),
+        style({transform:'translateY(-30%)'}),
+        style({transform:'translateY(-60%)'}),
+        style({transform:'translateY(-65%)'}),
+        style({transform:'translateY(-80%)'}),
+        style({transform:'translateY(-88%)'}),
+        style({transform:'translateY(-90%)'}),
+        style({transform:'translateY(0)'}),
+        style({transform:'translateY(20%)'}),
+        style({transform:'translateY(40%)'}),
+        style({transform:'translateY(50%)'}),
+        style({transform:'translateY(60%)'}),
+        style({transform:'translateY(90%)'}),
+        style({transform:'translateY(10%)'}),
+        style({transform:'translateY(-20%)'}),
+        style({transform:'translateY(-30%)'}),
+        style({transform:'translateY(-60%)'}),
+        style({transform:'translateY(-90%)'}),
+
+      ]))),
     ])
   ]
 })
