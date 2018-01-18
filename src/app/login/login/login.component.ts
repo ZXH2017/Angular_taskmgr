8b78243fdd1be04119d4f8b37369812737d783d5
ZXH2017 <564407380@qq.com>
2018-01-18 23:08:44 +0800

    登录的表单处理和自定义表单控件

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    //--------------这是复杂的方式--------------------
+    // this.form = new FormGroup({
+    //   //多个验证器  Vaildators.compose()
+    //   email: new FormControl('wang@163.com', Validators.compose([Validators.required, Validators.email])),
+    //   password: new FormControl('', Validators.required)
+    // })
+
+    //--------------这是简单的方式--------------------
+    this.form = this.fb.group({
+      //多个验证器  Vaildators.compose()   自定义表单控件也可应用在此1.this.validate
+      email: ['wan@local.dev', Validators.compose([Validators.required, Validators.email, this.validate])],
+      password: ['', Validators.required]
+    })
   }
 
+  onSubmit({ value, valid }, ev: Event) {
+    ev.preventDefault();
+    console.log(JSON.stringify(value));
+    console.log(valid);
+    // 自定义表单控件也可应用在此2.this.validate
+    // if(){....}
+    // this.form.controls['email'].setValidators(this.validate);
+  }
+
+  validate(c: FormControl): { [key: string]: any } {
+    if (!c.value) {
+      return null;
+    }
+    const pattern = /^wang+/;
+    if (pattern.test(c.value)) {
+      return null;
+    }
+    return {
+      emailNotVaild: 'The email must start with wang'
+    }
+  }
 }
