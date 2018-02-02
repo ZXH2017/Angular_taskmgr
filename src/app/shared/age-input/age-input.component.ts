5c6f7da384b2bdf8b9b7e6de985e2b760349486c
ZXH2017 <564407380@qq.com>
2018-02-03 00:08:46 +0800

    复杂控件二

--- src/app/shared/age-input/age-input.component.ts
+++ src/app/shared/age-input/age-input.component.ts
@@ -1,6 +1,31 @@
 import { Component, Input, forwardRef } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR, NG_VALIDATORS, FormControl, FormBuilder, FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
+// import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/observable/combineLatest';
+import 'rxjs/add/operator/distinctUntilChanged';
+
+import {
+  subDays,
+  subMonths,
+  subYears,
+  differenceInDays,
+  differenceInMonths,
+  differenceInYears,
+  isBefore,
+  parse
+} from 'date-fns';
+
+export enum AgeUnit {
+  Year = 0,
+  Month,
+  Day
+}
+
+export interface Age {
+  age: number;
+  unit: AgeUnit;
+}
 
 @Component({
   selector: 'app-age-input',
@@ -27,32 +52,79 @@ export class AgeInputComponent implements ControlValueAccessor {
 
   ngOnInit() {
     this.form = this.fb.group({
-      birthday: [],
+      birthday: ['', this.validateDate],
       age: this.fb.group({
         ageNum: [],
         ageUnit: []
-      })
+      }, { validator: this.validateAge('ageNum', 'ageUnit') })
     });
     const birthday = this.form.get('birthday');
     const ageNum = this.form.get('age').get('ageNum');
     const ageUnit = this.form.get('age').get('ageNUnit');
 
-    const birthday$ = birthday.valueChanges.map(d => {
-      return { date: d, form: 'birthday' };
-    });
+    const birthday$ = birthday.valueChanges
+      .map(d => {
+        return { date: d, form: 'birthday' };
+      })
+      .debounceTime(300)
+      .distinctUntilChanged()
+      .filter(_ => birthday.valid);//判断birthday的正确性
 
-    const ageNum$ = ageNum.valueChanges;
-    const ageUnit$ = ageUnit.valueChanges;
-    const age$ = Observable.combineLatest(ageNum$, ageUnit$, (_n, _u) => {
-      return this.toDate({ age: _n, unit: _u });
-    }).map(d => {
-      return { date: d, form: 'age' };
-    })
+    const ageNum$ = ageNum.valueChanges
+      .startWith(ageNum.value)
+      .debounceTime(300)
+      .distinctUntilChanged();//控件本身的值，startWidth是初始化
+    const ageUnit$ = ageUnit.valueChanges
+      .startWith(ageUnit.value)
+      .debounceTime(300)
+      .distinctUntilChanged();
+    const age$ = Observable
+      .combineLatest(ageNum$, ageUnit$, (_n, _u) => {
+        return this.toDate({ age: _n, unit: _u });
+      })
+      .map(d => {
+        return { date: d, form: 'age' };
+      })
+      .filter(_ => this.form.get('age').valid);//依赖age的formGroup的正确性
 
     //最終合并成：
-    const merged$ = Observable.merge(birthday$, age$)
+    const merged$ = Observable
+      .merge(birthday$, age$)
+      .filter(_ => this.form.valid);//过滤掉表单不合法的值
+    merged$.subscribe(d => {
+      const age = this.toAge(d.date);
+      if (d.form === 'birthday') {//日期转岁数
+        if (age.age != ageNum.value) {
+          ageNum.patchValue(age.age, { emitEvent: false });
+        }
+        if (age.unit != ageUnit.value) {
+          ageUnit.patchValue(age.unit, { emitEvent: false });
+        }
+        this.propagateChange(d.date);
+      } else {//岁数转日期
+        const ageToCompare = this.toAge(birthday.value);
+        if (age.age != ageToCompare.age || age.unit != ageToCompare.unit) {
+          birthday.patchValue(d.date, { emitEvent: false });
+          this.propagateChange(d.date);
+        }
+      }
+
+    })
 
   }
+
+  validate(c: FormControl): { [key: string]: any } {//做自定义表单控件整个的验证
+
+  }
+
+  validateDate(c: FormControl): { [key: string]: any } {
+
+  }
+
+  validateAge(ageNumKey: string, ageUnitKey: string): { [key: string]: any } {
+    
+  }
+  
   writeValue(obj: any): void { };
 
   registerOnChange(fn: any): void {
@@ -60,4 +132,37 @@ export class AgeInputComponent implements ControlValueAccessor {
   };
 
   registerOnTouched(fn: any): void { };
+
+  toAge(dateStr: string): Age {
+    const date = parse(dateStr);
+    const now = Date.now();
+    return isBefore(subDays(now, 90), date) ? //  如果now-90在date之前，就是90天之内
+      { age: differenceInDays(now, date), unit: AgeUnit.Day } :
+      isBefore(subMonths(now, 24), date) ?
+        { age: differenceInMonths(now, date), unit: AgeUnit.Month } :
+        {
+          age: differenceInYears(now, date),
+          unit: AgeUnit.Year
+        }
+  }
+
+  toDate(age: Age): string {
+    const now = Date.now();
+    const dateFormat = 'YYYY-MM-DD';
+    switch (age.unit) {
+      case AgeUnit.Year: {
+        return format(subYears(now, age.age), dateFormat);//format格式化成要的格式
+      }
+      case AgeUnit.Month: {
+        return format(subMonths(now, age.age), dateFormat);
+      }
+      case AgeUnit.Day: {
+        return format(subDays(now, age.age), dateFormat);
+      }
+      default: {
+        return null;
+      }
+    }
+
+  }
 }
